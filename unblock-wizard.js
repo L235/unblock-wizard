58df38f6b9feada091d606dd360f3cd978529554
Aasim Syed <65820963+Awesome-Aasim@users.noreply.github.com>
2025-07-17 10:15:33 +0500

    major changes, including using mediawiki system messages, adding support for preloads, removing UTRS button as that should be in wikitext, linking to Wikipedia:UTRS rather than the UTRS url in case UTRS is down

--- unblock-wizard.js
+++ unblock-wizard.js
@@ -15,21 +15,9 @@
 /* globals mw, $, OO */
 /* <nowiki> */
 
-(function () {
+(async function () {
 
-$.when(
-	$.ready,
-	mw.loader.using([
-		'mediawiki.util', 'mediawiki.api', 'mediawiki.Title',
-		'mediawiki.widgets', 'oojs-ui-core', 'oojs-ui-widgets'
-	])
-).then(function () {
-	if (!(mw.config.get('wgPageName').includes('Wikipedia:Unblock_wizard/') || mw.config.get('wgPageName').includes('User:Chaotic_Enby/Unblock_wizard/')) ||
-		mw.config.get('wgAction') !== 'view') {
-		return;
-	}
-	init();
-});
+
 
 var wizard = {}, ui = {}, block = {};
 window.wizard = wizard;
@@ -42,6 +30,8 @@ var config = {
 
 var demoMode = !!mw.util.getParamValue("demoMode");
 var usernameBlock = mw.util.getParamValue("usernameBlock");
+
+await new mw.Api().loadMessagesIfMissing(['wikimedia-copyrightwarning', 'copyrightwarning']);
 
 // TODO: move to a separate JSON subpage, would be feasible once [[phab:T198758]] is resolved
 var messages = {
@@ -57,11 +47,11 @@ var messages = {
 	"future-promo-label": "If you are unblocked, what topic areas will you edit in?",
 	"username-label": (usernameBlock == "required" ? "What new username do you want to pick?" : "If your username was an issue, what new username do you want to pick?"),
 	"standalone-username-label": "What new username do you want to pick?",
+	"additional-reason-label": "Additional reason to be unblocked",
 	"clarification-label": "Is there anything specific you want to ask about your block?",
 	"submit-label": "Submit",
-	"utrs-label": "Go to UTRS",
-	"utrs-necessary": "It is necessary to appeal your block via UTRS. This is because your talk page access is disabled. [https://utrs-beta.wmflabs.org/public/appeal/account Go to UTRS]",
-	"utrs-necessary-confirm": "It is necessary to appeal your block via UTRS. This is because your talk page access is disabled. Select \"Confirm\" to go to UTRS.",
+	"utrs-necessary": "It is necessary to appeal your block via UTRS. This is because your talk page access is disabled. [[Wikipedia:UTRS|Learn more about UTRS]]]",
+	"utrs-necessary-confirm": "It is necessary to appeal your block via UTRS. This is because your talk page access is disabled. Select \"Confirm\" to learn more about UTRS.",
 	"footer-text": "<small>If you are not sure about what to enter in a field, you can skip it. If you need help, you can ask on <b>[[Special:MyTalk|your talkpage]]</b> with <b>{{[[Template:Help me|Help me]]}}</b> or get live help via <b>[[WP:IRCHELP|IRC]]</b>.<br>Facing some issues in using this form? <b>[/w/index.php?title=Wikipedia_talk:Unblock_wizard&action=edit&section=new&preloadtitle=Issue%20with%20submission%20form&editintro=Wikipedia_talk:Unblock_wizard/editintro Report it]</b>.</small>",
 	"submitting-as": "Submitting as User:$1",
 	"validation-notitle": "User not found",
@@ -75,13 +65,13 @@ var messages = {
 	"status-redirecting-utrs": "Redirecting you to UTRS ...",
 	"status-not-blocked": "You are not currently blocked.",
 	"status-not-blocked-confirm": "You are not currently blocked. Select \"OK\" to activate demo mode, which will allow you to check out the workflow without posting a block request.",
-	"status-error": "Due to an error, your unblock request could not be parsed. You can try to submit an unblock request manually by pasting the following on [[Special:MyTalk|your talk page]]:<br /><code>$1</code><br />If you are having difficulties, please [https://utrs-beta.wmflabs.org/ make a request through UTRS] and inform them of the issues you are encountering.",
+	"status-error": "Due to an error, your unblock request could not be parsed. You can try to submit an unblock request manually by {{#if:$2|[$2 clicking this link] or}} pasting the following on [[Special:MyTalk|your talk page]]:<br /><code>$1</code><br />If you are having difficulties, please [[Wikipedia:UTRS|make a request through UTRS]] and inform them of the issues you are encountering.",
 	"captcha-label": "Please enter the letters appearing in the box below",
 	"captcha-placeholder": "Enter the letters here",
 	"captcha-helptip": "CAPTCHA security check. Click \"Submit\" again when done.",
 	"error-saving-main": "An error occurred ($1). Please try again or ask for help on your talk page.",
 	"error-main": "An error occurred ($1). Please try again or ask for help on your talk page.",
-	"copyright-notice": "<small>By publishing changes, you agree to the [[:foundation:Special:MyLanguage/Policy:Terms of Use|Terms of Use]], and you irrevocably agree to release your contribution under the [[Wikipedia:Text of the Creative Commons Attribution-ShareAlike 4.0 International License|CC BY-SA 4.0 License]] and the [[Wikipedia:Text of the GNU Free Documentation License|GFDL]]. You agree that a hyperlink or URL is sufficient attribution under the Creative Commons license.</small>"
+	"copyright-notice": `<small>${mw.message('wikimedia-copyrightwarning').plain()}</small>`,
 };
 
 // var infoLevels = {
@@ -94,8 +84,8 @@ var messages = {
 // };
 
 var questionLabels = [];
-var questionFields = {'explain': 0, 'future': 0, 'other': 0, 'accounts': 0, 'so': 2, 'explain-promo': 0, 'coi': 0, 'future-promo': 0, 'username': 1, 'clarification': 0, 'standalone-username': 1};
-var required = {'explain': true, 'future': true, 'other': false, 'accounts': true, 'so': true, 'explain-promo': true, 'coi': true, 'future-promo': true, 'username': (usernameBlock == "required"), 'clarification': false, 'standalone-username': true};
+var questionFields = {'explain': 0, 'future': 0, 'other': 0, 'accounts': 0, 'so': 2, 'explain-promo': 0, 'coi': 0, 'future-promo': 0, 'username': 1, 'clarification': 0, 'standalone-username': 1, 'additional-reason': 0};
+var required = {'explain': true, 'future': true, 'other': false, 'accounts': true, 'so': true, 'explain-promo': true, 'coi': true, 'future-promo': true, 'username': (usernameBlock == "required"), 'additional-reason': false, 'clarification': false, 'standalone-username': true};
 
 var blockType = '';
 var emptyFields = false;
@@ -156,7 +146,7 @@ function init() {
 				questionLabels = ['clarification'];
 				break;
 			case "Username":
-				questionLabels = ['standalone-username'];
+				questionLabels = ['standalone-username', 'additional-reason'];
 				break;
 			default:
 				questionLabels = [];
@@ -240,7 +230,7 @@ function constructUI() {
 	}
 	
 	ui.itemsLayout.push(ui.submitLayout = new OO.ui.FieldLayout(ui.submitButton = new OO.ui.ButtonWidget({
-		label: blockType == "IP_hardblock" ? msg('utrs-label') : msg('submit-label'),
+		label: msg('submit-label'),
 		flags: [ 'progressive', 'primary' ],
 	})));
 	
@@ -292,7 +282,7 @@ function constructUI() {
 		ui.submitButton.setDisabled(true);
 		setMainStatus('error', msg('utrs-necessary'));
 		if (confirm(msg("utrs-necessary-confirm"))) {
-			location.href = "https://utrs-beta.wmflabs.org/public/appeal/account";
+			location.href = "/wiki/Wikipedia:UTRS";
 		}
 	} else {
 		ui.submitButton.on('click', handleSubmit);
@@ -363,10 +353,6 @@ function errorsFromPageData(page) {
 	return [];
 }
 
-function imglink(img) {
-	return '<img src="https://upload.wikimedia.org/wikipedia/commons/' + img[0] + '/' + img[1] + '/40px-' + img[1] + '.png" decoding="async" width="30" height="30" class="mw-file-element" srcset="https://upload.wikimedia.org/wikipedia/commons/thumb/' + img[0] + '/' + img[1] + '/60px-' + img[1] + '.png 1.5x">';
-}
-
 /**
  * @param {string} type
  * @param {string} message
@@ -395,74 +381,67 @@ function handleSubmit() {
 	ui.submitButton.setDisabled(true);
 	ui.mainLabel.scrollElementIntoView();
 	
+	var url = prepareUserTalkPageLink();
 	var text = prepareUserTalkText();
-	if (blockType == "IP_hardblock") {
-		setMainStatus('', msg('status-redirecting-utrs'));
-		$(window).off('beforeunload', wizard.beforeUnload);
-		setTimeout(function () {
-			location.href = "https://utrs-beta.wmflabs.org/public/appeal/account";
-		}, config.redirectionDelay);
+	for(var [i, label] of questionLabels.entries()){
+		if(required[label] && !ui.itemsInput[i].getValue()){
+			emptyFields = true;
+		}
+	}
+	if (emptyFields && !emptyFieldsWarned) {
+		setMainStatus('warning', msg('status-blank'));
+		emptyFieldsWarned = true;
+		ui.submitButton.setDisabled(false);
 	} else {
-		for(var [i, label] of questionLabels.entries()){
-			if(required[label] && !ui.itemsInput[i].getValue()){
-				emptyFields = true;
-			}
-		}
-		if (emptyFields && !emptyFieldsWarned) {
-			setMainStatus('warning', msg('status-blank'));
-			emptyFieldsWarned = true;
+		var userTalk = "User talk:" + block.target;
+		if (!block.target) { // empty
+			ui.fieldset.removeItems([ui.mainLabel]);
 			ui.submitButton.setDisabled(false);
-		} else {
-			var userTalk = "User talk:" + block.target;
-			if (!block.target) { // empty
-				ui.fieldset.removeItems([ui.mainLabel]);
-				ui.submitButton.setDisabled(false);
-				return; // really get the ip please
-			}
-		
-			wizard.api.get({
-				"action": "query",
-				"prop": "revisions|description",
-				"titles": userTalk,
-				"rvprop": "content",
-				"rvslots": "main",
-			}).then(function (json) {
-				var apiPage = json.query.pages[0];
-		
-				var errors = errorsFromPageData(apiPage);
-				if (errors.length) {
-					// ui.fieldset.removeItems([ui.mainLabel]);
-					ui.submitButton.setDisabled(false);
-					setMainStatus('error', msg('status-error').replace("$1", text));
-					return;
-				}
-		
-				setMainStatus('', msg('status-saving'));
-				if (demoMode) {
-					setMainStatus('success', '<code style="display: block">' + text + '</code>');
-				} else {
-					saveUserTalkPage(userTalk, apiPage.revisions[0].slots.main.content + text).then(function () {
-						setMainStatus('success', msg('status-redirecting'));
-			
-						$(window).off('beforeunload', wizard.beforeUnload);
-						setTimeout(function () {
-							location.href = mw.util.getUrl(userTalk);
-						}, config.redirectionDelay);
-					}, function (code, err) {
-						if (code === 'captcha') {
-							ui.fieldset.removeItems([ui.mainLabel, ui.talkStatusLayout]);
-							ui.captchaLayout.scrollElementIntoView();
-						} else {
-							setMainStatus('error', msg('status-error').replace('$1', text));
-						}
-						ui.submitButton.setDisabled(false);
-					});
-				}
-			}).catch(function (code, err) {
-				setMainStatus('error', msg('status-error').replace("$1", text));
-				ui.submitButton.setDisabled(false);
-			});
+			return; // really get the ip please
 		}
+	
+		wizard.api.get({
+			"action": "query",
+			"prop": "revisions|description",
+			"titles": userTalk,
+			"rvprop": "content",
+			"rvslots": "main",
+		}).then(function (json) {
+			var apiPage = json.query.pages[0];
+	
+			var errors = errorsFromPageData(apiPage);
+			if (errors.length) {
+				// ui.fieldset.removeItems([ui.mainLabel]);
+				ui.submitButton.setDisabled(false);
+				setMainStatus('error', msg('status-error').replace("$1", text).replace('$2', url));
+				return;
+			}
+	
+			setMainStatus('', msg('status-saving'));
+			if (demoMode) {
+				setMainStatus('success', 'Wikitext: <code style="display: block">' + text + '</code>\n\nPreload URL: <a href=\"' + url + '\" target=\"_blank\">' + url.replace("&", "&amp;").replace("<", "&lt;") + '</a>');
+			} else {
+				saveUserTalkPage(userTalk, apiPage.revisions[0].slots.main.content + text).then(function () {
+					setMainStatus('success', msg('status-redirecting'));
+		
+					$(window).off('beforeunload', wizard.beforeUnload);
+					setTimeout(function () {
+						location.href = mw.util.getUrl(userTalk);
+					}, config.redirectionDelay);
+				}, function (code, err) {
+					if (code === 'captcha') {
+						ui.fieldset.removeItems([ui.mainLabel, ui.talkStatusLayout]);
+						ui.captchaLayout.scrollElementIntoView();
+					} else {
+						setMainStatus('error', msg('status-error').replace('$1', text).replace('$2', url));
+					}
+					ui.submitButton.setDisabled(false);
+				});
+			}
+		}).catch(function (code, err) {
+			setMainStatus('error', msg('status-error').replace("$1", text).replace("$2", url));
+			ui.submitButton.setDisabled(false);
+		});
 	}
 }
 
@@ -510,6 +489,55 @@ function saveUserTalkPage(title, text) {
 	});
 }
 
+function prepareUserTalkPageLink() {
+	var url = new URL(location.origin + mw.config.get("wgArticlePath").replace("$1", "User_talk:" + mw.config.get("wgUserName")));
+	url.searchParams.set("action", "edit");
+	url.searchParams.set("section", "new");
+	switch(blockType){
+		case "Autoblock":
+			if("id" in block){
+				url.searchParams.set("editintro", `Template:Unblock-auto/editintro`);
+				url.searchParams.set("preloadtitle", "Autoblock appeal");
+				url.searchParams.set("preload", `Template:Unblock-auto/preload`);
+				url.searchParams.set("preloadparams[0]", block.reason);
+				url.searchParams.set("preloadparams[1]", block.by);
+				url.searchParams.set("preloadparams[2]", block.id);
+			}
+			break;
+		case "Clarification":
+			url.searchParams.set("preload", `Help:Contents/helpmepreload`);
+			url.searchParams.set("preloadparams[0]", ui.itemsInput[0].getValue());
+			url.searchParams.set("preloadparams[1]", "");
+			break;
+		case "Username":
+			url.searchParams.set("editintro", `Template:Unblock-un/editintro`);
+			url.searchParams.set("preloadtitle", "Unblock request for change in username");
+			url.searchParams.set("preload", `Template:Unblock-un/preload`);
+			url.searchParams.append("preloadparams[0]", ui.itemsInput[1] ? ui.itemsInput[1].getValue() : '');
+			url.searchParams.append("preloadparams[1]", ui.itemsInput[0].getValue());
+			break;
+		default:
+			url.searchParams.set("editintro", `Template:Unblock/editintro`);
+			url.searchParams.set("preloadtitle", "Unblock request");
+			url.searchParams.set("preload", `Template:Unblock/preload`);
+			var reason = '';
+			for(var [i, label] of questionLabels.entries()){
+				if(required[label] || ui.itemsInput[i].getValue()) {
+					if(label == "username") {
+						url.searchParams.set("editintro", `Template:Unblock-un/editintro`);
+						url.searchParams.set("preloadtitle", "Unblock request with change in username");
+						url.searchParams.set("preload", `Template:Unblock-un/preload`);
+						url.searchParams.append("preloadparams[1]", ui.itemsInput[i].getValue());
+					} else {
+						reason += "'''''" + msg(label + '-label') + "'''''" + "{{pb}}" + ui.itemsInput[i].getValue() + "{{pb}}";
+					}
+				}
+			}
+			url.searchParams.append("preloadparams[0]", reason);
+	}
+	return url.toString();
+}
+
 /**
  * @param {Object} page - page information from the API
  * @returns {string} final talk page text to save
@@ -534,7 +562,7 @@ function prepareUserTalkText() {
 			}
 			break;
 		case "Username":
-			unblock += '\n{{unblock-un|username=' + ui.itemsInput[0].getValue() + ' ~~' + '~~ }}\n';
+			unblock += `\n{{unblock-un|1=${ui.itemsInput[0].getValue()}|reason=${ui.itemsInput[1] ? ui.itemsInput[1].getValue() : ''}}} \n`;
 			break;
 		default:
 			unblockStart = '\n{{unblock|reason=';
@@ -542,7 +570,7 @@ function prepareUserTalkText() {
 			for(var [i, label] of questionLabels.entries()){
 				if(required[label] || ui.itemsInput[i].getValue()) {
 					if(label == "username") {
-						unblockStart = '\n{{unblock-un|username=' + ui.itemsInput[i].getValue() + '|reason=';
+						unblockStart = '\n{{unblock-un|1=' + ui.itemsInput[i].getValue() + '|reason=';
 					} else {
 						unblock += "'''''" + msg(label + '-label') + "'''''" + "{{pb}}" + ui.itemsInput[i].getValue() + "{{pb}}";
 					}
@@ -627,6 +655,21 @@ function debug() {
 		console.log(arg);
 	});
 }
+
+
+await $.when(
+	$.ready,
+	mw.loader.using([
+		'mediawiki.util', 'mediawiki.api', 'mediawiki.Title',
+		'mediawiki.widgets', 'oojs-ui-core', 'oojs-ui-widgets'
+	])
+);
+
+if (!(mw.config.get('wgPageName').includes('Wikipedia:Unblock_wizard/')) ||
+	mw.config.get('wgAction') !== 'view') {
+	return;
+}
+init();
 
 })(); // File-level closure to protect functions from being exposed to the global scope or overwritten
 
